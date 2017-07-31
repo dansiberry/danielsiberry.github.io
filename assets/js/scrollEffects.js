019a8a8dc03a73a09a7f02b5a0801b1744b7ea11
Dan Siberry <dansiberry1@gmail.com>
2017-07-31 13:37:30 +0100

    second commit

--- assets/js/scrollEffects.js
+++ assets/js/scrollEffects.js
@@ -5,6 +5,7 @@ window.domLoaded = function domLoaded() {
   let landing = document.getElementById("landing")
   let citizenVoice = document.getElementById("citizenvoice")
   let pages = document.getElementsByClassName("scroll-page")
+  let home = document.getElementById("home")
   let pageCounter = 0
 
   pageDownButton.addEventListener("click", pageDown)
@@ -18,6 +19,8 @@ window.domLoaded = function domLoaded() {
       let currentPage2 = pages[pageCounter + 1]
       let pageBelow1 = pages[pageCounter + 2]
       let pageBelow2 = pages[pageCounter + 3]
+      home.classList.remove("background-" + (pageCounter / 2 + 1))
+      home.classList.add("background-" + (pageCounter / 2))
       pageBelow1.classList.remove("in-view")
       pageBelow1.classList.add("to-be-viewed")
       pageBelow2.classList.remove("in-view")
@@ -30,13 +33,15 @@ window.domLoaded = function domLoaded() {
   }
 
   function pageDown() {
-    if (pageCounter != (pages.length - 1)){
+    if (pageCounter != (pages.length - 2)){
       pageCounter ++
       pageCounter ++
       let currentPage1 = pages[pageCounter]
       let currentPage2 = pages[pageCounter + 1]
       let pageAbove1 = pages[pageCounter - 1]
       let pageAbove2 = pages[pageCounter - 2]
+      home.classList.remove("background-" + (pageCounter / 2 - 1))
+      home.classList.add("background-" + (pageCounter / 2))
       pageAbove1.classList.remove("in-view")
       pageAbove1.classList.add("viewed")
       pageAbove2.classList.remove("in-view")
@@ -45,6 +50,8 @@ window.domLoaded = function domLoaded() {
       currentPage1.classList.add("in-view")
       currentPage2.classList.remove("to-be-viewed")
       currentPage2.classList.add("in-view")
+      console.log(pageCounter)
+      console.log(pages.length)
     }
   }
 
